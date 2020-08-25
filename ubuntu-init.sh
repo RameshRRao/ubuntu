9965a3057f47c2a23c1935cea9d3aaeae348dcab
RameshRRao <69587495+RameshRRao@users.noreply.github.com>
2020-08-25 17:47:32 -0500

    Create ubuntu-init.sh
    typical initializations

---
+++ ubuntu-init.sh
@@ -0,0 +1,5 @@
+sudo apt update && sudo apt upgrade
+sudo apt install htop vim wget curl
+wget https://raw.githubusercontent.com/RameshRRao/ubuntu/master/.bash_aliases
+wget https://raw.githubusercontent.com/RameshRRao/ubuntu/master/.vimrc
+sudo touch /etc/cloud/cloud-init.disabled
